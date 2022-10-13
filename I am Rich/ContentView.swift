744d5ec0d54c9c55eb180a7acdba8700804b0443
松本詞音 <98865688+xion0207@users.noreply.github.com>
2022-10-13 11:55:04 +0900

    Initial Commit

---
+++ I am Rich/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  I am Rich
+//
+//  Created by 松本詞音 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by 松本詞音 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
